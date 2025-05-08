f3d56128fbea3dd86c70c46fb1c33acb4004217a
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2025-05-08 11:54:37 +0200

    fix(staticpage): Set nextjs to static.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import nextra from 'nextra'
+import type { NextConfig } from 'next'
 
-// Set up Nextra with its configuration
 const withNextra = nextra({
-  // ... Add Nextra-specific options here
+  search: { codeblocks: false }
 })
- 
-// Export the final Next.js config with Nextra included
-export default withNextra({
-  // ... Add regular Next.js options here
-})
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  }
+}
+
+export default withNextra(nextConfig)
