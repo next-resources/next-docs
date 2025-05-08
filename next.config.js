2b5c946694fbe7d48dd3858e00c93312f55e2c5f
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2025-05-08 13:04:49 +0200

    fix(): building failed.

--- next.config.js
+++ next.config.js
@@ -2,14 +2,18 @@
 import nextra from 'nextra'
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
+
 })
 
 const nextConfig = {
   output: 'export',
   basePath: "/next-docs",
   // optioneel, alleen als je mdx-components gebruikt
+  turbopack: {
+    resolveAlias: {
+      'next-mdx-import-source-file': './src/mdx-components.js'
+    }
+  },
   images: {
     unoptimized: true,
   },
