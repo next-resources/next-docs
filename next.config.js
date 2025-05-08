ac27749704bc06ac1a0773a2ffd91ca7802698bf
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2025-05-08 13:07:09 +0200

    fix(): building failed.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 import nextra from 'nextra'
  
-// Set up Nextra with its configuration
 const withNextra = nextra({
-  // ... Add Nextra-specific options here
+
 })
- 
-// Export the final Next.js config with Nextra included
+
 export default withNextra({
   output: 'export',
-  basePath: "/next-docs",
-  // optioneel, alleen als je mdx-components gebruikt
-  turbopack: {
-    resolveAlias: {
-      'next-mdx-import-source-file': './src/mdx-components.js'
-    }
-  },
   images: {
     unoptimized: true,
   },
