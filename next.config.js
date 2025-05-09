0d62f110264a961f1ed2f45144ab09ad211c3859
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2025-05-09 10:25:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const withNextra = require('nextra')({
 
 module.exports = {
   ...withNextra(),
-  basePath: '/next-docs',
-  assetPrefix: '/next-docs/',
   images: { unoptimized: true },
   trailingSlash: true,
 };
