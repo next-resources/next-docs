13c5a229ad3c5416218a98a2810c0698666cfc78
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2025-05-09 10:29:13 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 });
 
