67e97a6820bd2d685958e1ed4fa2911760a7a154
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2025-05-09 10:30:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-});
+})
 
 module.exports = withNextra({
-  output: 'export',            // 💥 required for static export
-  trailingSlash: true,
-  images: { unoptimized: true },
-});
+  basePath: '../',
+  assetPrefix: '../',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+})
